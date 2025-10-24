8cf1e5b0df17a84dd49b2f6ca0a6ad3396d8757d
fredballer90-ops <fredballer90@gmail.com>
2025-10-24 04:38:18 +0300

    Fixed Firebase admin duplication and config setup

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,16 +1,19 @@
 import admin from "firebase-admin";
 
+const privateKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n');
+
 const serviceAccount = {
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey,
 };
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    databaseURL: "https://trendbet-c2793.firebaseio.com", // change if you use Realtime DB
   });
 }
 
-export default admin;
+export const db = admin.firestore();
+
